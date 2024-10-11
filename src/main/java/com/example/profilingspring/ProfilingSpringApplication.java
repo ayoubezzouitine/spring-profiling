049817002d4c7ca3559d9c25b72981c3cb6344ca
EZZOUITINE Ayoub <blue.ayoub@gmail.com>
2024-10-11 20:40:47 +0200

    Initial commit for Spring Boot project

---
+++ src/main/java/com/example/profilingspring/ProfilingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.profilingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilingSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfilingSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/profilingspring/ProfilingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profilingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilingSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
