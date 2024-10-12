8d857171b44db21b644d0bee23364d117c551c25
EZZOUITINE Ayoub <blue.ayoub@gmail.com>
2024-10-12 08:47:30 +0200

    Fist exemple of creating profiling using: - 3 profils with different apps properties for each profil - DEV, PreProd and Prod - in our main apps properties i active the profil to be load with his dipedendancies

--- src/main/java/com/example/profilingspring/ProfilingSpringApplication.java
+++ src/main/java/com/example/profilingspring/ProfilingSpringApplication.java
@@ -1,13 +1,23 @@
 package com.example.profilingspring;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProfilingSpringApplication {
+public class ProfilingSpringApplication implements CommandLineRunner {
+
+    @Value("${message}")
+    private String message;
+
 
     public static void main(String[] args) {
         SpringApplication.run(ProfilingSpringApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+              System.out.println(message);
+    }
 }
